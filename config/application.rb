59d94c18642410ebf0cbcf81e26bca7e27cfb976
st118173 <st118173@ait.asia>
2017-05-14 04:49:50 +0700

    aa

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
-config.autoload_paths += "#{Rails.root}/app/uploaders"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,6 +13,7 @@ module Maa
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += "#{Rails.root}/app/uploaders"
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.enabled = false
   end
