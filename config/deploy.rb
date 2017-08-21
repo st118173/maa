7c9d1f75891345f8a8467f929baf0a753c021333
st118173 <st118173@ait.asia>
2017-08-21 21:55:15 +0700

    deploy to jntu

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,7 +95,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :scm,           :git
 set :branch,        :master
 set :format,        :pretty
-set :log_level,     :debug
+#set :log_level,     :debug
 set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
