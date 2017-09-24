84759f9447b5e94d4c6e11ef75cfbf6b71847afd
st118173 <st118173@ait.asia>
2017-09-24 15:50:28 +0530

    d

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,7 +77,7 @@ set :rbenv_path, '/home/deploy/.rbenv/'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        true
+#set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
